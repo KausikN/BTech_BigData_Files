2efa51af65e5b2af8dcaf9dcc6d188ca2f633189
Kausik N <nkausik1999@gmail.com>
2020-04-19 18:45:58 +0530

    added more answers

--- ReadingAssignment 1/test.py
+++ ReadingAssignment 1/test.py
@@ -1,14 +1,37 @@
 import numpy as np
-from scipy import optimize
 
-# Function
-def F(x):
-    v = ((x**3) -2*(x**2) + x)
-    if v == 0:
-        return 1
-    return (1/((x**3) -2*(x**2) + x))
+items = [(3, 4), (7, 5), (2, 6), (3, 1),
+        (8, 2), (7, 3), (4, 4), (6, 6),
+        (7, 4), (6, 7)]
 
-# Genetic Optimisation
-print(optimize.minimize(F, x0=0))
-print(optimize.minimize(F, x0=0, method="L-BFGS-B"))
-quit()
+
+def AgglomerativeClustering(items):
+    cluster_points = []
+    cluster_names = []
+
+    # Initially all points are clusters
+    index = 0
+    for item in items:
+        cluster_points.append(item)
+        cluster_names.append([index])
+        index += 1
+    
+    # Find proximity matrix
+    Prox_Matrix = ProximityMatrix(cluster_points)
+
+    # Merge Nearest Clusters
+    cluster_names_new = cluster_names.copy()
+    cluster_points_new = cluster_points.copy()
+    merged_indices = []
+    merged_values = []
+    for i in range(len(cluster_points)):
+        closest_index = i+1
+        closest_value = -1
+        for j in range(i+1, len(cluster_points)):
+            if closest_value == -1 or Prox_Matrix[str(i) + "_" + str(j)] < closest_value:
+                closest_index = j
+                closest_value = Prox_Matrix[str(i) + "_" + str(j)]
+        if not closest_index in merged_indices:
+
+
+                
